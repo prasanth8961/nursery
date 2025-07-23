b07e39f4e965f52c563df9990d1ea72fe55e7e30
prasanth8961 <prasanthprs143@gmail.com>
2025-07-23 10:18:43 +0530

    FIX: next.configs-remotepatterns

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
+
   env: {
     SECRET_KEY: process.env.SECRET_KEY,
   },
